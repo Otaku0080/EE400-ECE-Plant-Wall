a1686db135d5c3d306e5a2b0d3181b4f76595ffc
galyapetrova <galine.petrovoy@gmail.com>
2023-11-09 13:28:59 -0800

    fixed rgb back, adding to uv

--- UVLights.py
+++ UVLights.py
@@ -1,7 +1,8 @@
 # this is code for operating the 14 UV lights on the Plant Wall
 # project for EE400
 # we want to connect all the UV lights to power and connect them
-# to an array of switches and we only close the switches
+# to transistors which are then connected to PINS
+# an array of switches and we only close the switches
 # (and therefore connect to ground and enable current flow)
 #  for 12 hours and then disconnect for 12 hours
 
@@ -11,24 +12,26 @@ import time
 # Set the GPIO mode to BCM
 GPIO.setmode(GPIO.BCM)
 
-# Define the GPIO pin for the LED
-UV_pin = 18
+# Define the GPIO pin array for the UV
+UV_pin_array = [18, 19, 20]
 
 # Set the LED pin as an output
-GPIO.setup(UV_pin, GPIO.OUT)
+for i in UV_pin_array:
+    GPIO.setup(UV_pin_array[i], GPIO.OUT)
+
 
 try:
     while True:
-        # Turn the LED on
-        GPIO.output(UV_pin, GPIO.HIGH)
-        print("LED ON")
+        # Turn the UV on
+        GPIO.output(UV_pin, GPIO.LOW)
+        print("UV ON")
         
         # Wait for 12 hours
         time.sleep(12 * 60 * 60)  # 12 hours
         
-        # Turn the LED off
-        GPIO.output(UV_pin, GPIO.LOW)
-        print("LED OFF")
+        # Turn the UV off
+        GPIO.output(UV_pin, GPIO.HIGH)
+        print("UV OFF")
         
         # Wait for 12 hours
         time.sleep(12 * 60 * 60)  # 12 hours
